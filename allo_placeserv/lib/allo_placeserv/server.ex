3509bd5c3e1d71af6a97253cbc56fbe9dae75703
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2018-10-13 21:19:23 +0200

    Handle mm crashes properly

--- allo_placeserv/lib/allo_placeserv/server.ex
+++ allo_placeserv/lib/allo_placeserv/server.ex
@@ -1,3 +1,11 @@
+defmodule ServerState do
+  defstruct clients: %{} # client_id to ClientRef
+end
+defmodule ClientRef do
+  @enforce_keys [:pid, :monitor, :id]
+  defstruct pid: nil, monitor: nil, id: nil
+end
+
 defmodule AlloPlaceserv.Server do
   @moduledoc """
   I think this class starts out handling network and manipulating the world state, while
@@ -12,21 +20,30 @@ defmodule AlloPlaceserv.Server do
   end
 
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, {
+    GenServer.start_link(__MODULE__, %ServerState{
+      
     }, opts)
   end
   
-  def handle_info({:new_client, client_id, _mm_pid}, state) do
-    :ok = AlloPlaceserv.PlaceStore.add_entity(AlloPlaceserv.Store, %Entity{
-      id: client_id
-    })
-      #todo: store clients in a hashmap
+  def handle_info({:new_client, client_id, mm_pid}, state) do
+    Logger.info("Client connected: #{client_id}")
+    {:ok, state} = add_client(client_id, mm_pid, state)
     {:noreply, state}
   end
   
   def handle_info({:lost_client, client_id}, state) do
-    AlloPlaceserv.PlaceStore.remove_entity(AlloPlaceserv.Store, client_id)
-    #todo: remove client from hashmap
+    Logger.info("Client disconnected: #{client_id}")
+    {:ok, state} = remove_client(client_id, state)
+    {:noreply, state}
+  end
+  def handle_info({:DOWN, _monitor_ref, :process, _mm_pid, reason}, state) when reason == :normal do
+    # handled by lost_client. this message is only sent because the monitor isn't removed in time.
+    {:noreply, state}
+  end
+  def handle_info({:DOWN, _monitor_ref, :process, mm_pid, reason}, state) do
+    {_client_id, client} = Enum.find(state.clients, fn({_key, client}) -> client.pid == mm_pid end)
+    Logger.info("Client crashed: #{client.id} reason: #{inspect(reason)}")
+    {:ok, state} = remove_client(client.id, state)
     {:noreply, state}
   end
   
@@ -36,4 +53,32 @@ defmodule AlloPlaceserv.Server do
   end
   
   #todo: handle sending world state with timer
+  
+  
+  ### Privates
+  defp add_client(client_id, mm_pid, state) do
+    monitorref = Process.monitor(mm_pid)
+    :ok = AlloPlaceserv.PlaceStore.add_entity(AlloPlaceserv.Store, %Entity{
+      id: client_id
+    })
+    {
+      :ok,
+      %ServerState{state|
+        clients: Map.put(state.clients, client_id, %ClientRef{
+          pid: mm_pid, monitor: monitorref,id: client_id
+        })
+      }
+    }
+  end
+  defp remove_client(client_id, state) do
+    {:ok, clientref} = Map.fetch(state.clients, client_id)
+    Process.demonitor(clientref.monitor)
+    :ok = AlloPlaceserv.PlaceStore.remove_entity(AlloPlaceserv.Store, client_id)
+    {
+      :ok,
+      %ServerState{state|
+        clients: Map.delete(state.clients, client_id)
+      }
+    }
+  end
 end
