8321ada1a2a0ad5234becbe8d2132a5b69430e1e
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2019-07-10 21:19:44 +0200

    Bump to elixir 1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AlloPlaceserv.MixProject do
     [
       app: :allo_placeserv,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       compilers: [:cmake] ++ Mix.compilers(),
       deps: deps()
