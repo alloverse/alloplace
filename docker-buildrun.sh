403bbc355235e2dda6a9b5c008cc80299a2b67e5
Nevyn Bengtsson <nevyn@alloverse.com>
2019-07-10 19:35:05 +0000

    allow naming docker-run places

--- docker-buildrun.sh
+++ docker-buildrun.sh
@@ -2,4 +2,4 @@
 
 rm -rf _cmake _build
 sudo docker build -t alloplace .
-sudo docker run -d -p 21337:21337/udp alloplace:latest
+sudo docker run -d -e ALLOPLACE_NAME -p 21337:21337/udp alloplace:latest
