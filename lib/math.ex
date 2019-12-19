5d84528573c355533d25bf90aa2e69c6240ec39c
nevyn Bengtsson <nevyn.jpg@gmail.com>
2019-12-19 10:40:48 +0100

    remove unused file

--- lib/math.ex
+++
@@ -1,8 +0,0 @@
-defmodule Allomath do
-  def g2avec({x, y, z}) do
-    %AlloVector{x: x, y: y, z: z}
-  end
-  def a2gvec(%AlloVector{x: x, y: y, z: z}) do
-    Graphmath.Vec3.create(x, y, z)
-  end
-end
