252bc703af558d10d72fd40898556c3d39f12032
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-03-18 16:47:27 +0100

    update_entity in statedaemon

--- src/state.c
+++ src/state.c
@@ -28,6 +28,17 @@ static void add_entity(long reqId, cJSON *json, ei_x_buff *response)
     ei_x_format_wo_ver(response, "{response, ~l, ok}", reqId);
 }
 
+static void update_entity(long reqId, const char *entity_id, cJSON *comps, ei_x_buff *response)
+{
+    allo_entity *entity = state_get_entity(&state, entity_id);
+    cJSON *comp = NULL;
+    cJSON_ArrayForEach(comp, comps) {
+        cJSON_ReplaceItemInObject(entity->components, comp->string, comp);
+    }
+
+    ei_x_format_wo_ver(response, "{response, ~l, ok}", reqId);
+}
+
 static void simulate(long reqId, double dt, cJSON *jintents, ei_x_buff *response)
 {
     int intent_count = cJSON_GetArraySize(jintents);
@@ -125,28 +136,46 @@ void handle_erl()
     assert(ei_decode_tuple_header(request, &request_index, &argsLen) == 0);
     
     scopedx ei_x_buff response; ei_x_new_with_version(&response);
-    if(strcmp(command, "ping") == 0) {
+    if(strcmp(command, "ping") == 0)
+    {
         ei_x_format_wo_ver(&response, "{response, ~l, statepong}", reqId);
-    } else if(strcmp(command, "add_entity") == 0) {
+    }
+    else if(strcmp(command, "add_entity") == 0)
+    {
         cJSON *json = ei_decode_cjson_string(request, &request_index);
         add_entity(reqId, json, &response);
-    } else if(strcmp(command, "simulate") == 0) {
+    }
+    else if(strcmp(command, "update_entity") == 0)
+    {
+        char *entity_id = ei_decode_elixir_string(request, &request_index);
+        cJSON *json = ei_decode_cjson_string(request, &request_index);
+        update_entity(reqId, entity_id, json, &response);
+    }
+    else if(strcmp(command, "simulate") == 0)
+    {
         double dt;
         assert(ei_decode_double(request, &request_index, &dt) == 0);
         cJSON *json = ei_decode_cjson_string(request, &request_index);
         simulate(reqId, dt, json, &response);
-    } else if(strcmp(command, "get_snapshot") == 0) {
+    }
+    else if(strcmp(command, "get_snapshot") == 0)
+    {
         get_snapshot(reqId, &response);
-    } else if(strcmp(command, "get_owner_id") == 0) {
+    }
+    else if(strcmp(command, "get_owner_id") == 0)
+    {
         assert(argsLen == 1);
         char *owner_id = ei_decode_elixir_string(request, &request_index);
         get_owner_id(reqId, owner_id, &response);
         free(owner_id);
-    } else {
+    }
+    else
+    {
         printf("statedaemon: Unknown command %s\n", command);
         ei_x_format_wo_ver(&response, "{response, ~l, {error, \"no such command\"}}", reqId);
     }
-    if(response.index == 0) {
+    if(response.index == 0)
+    {
         printf("statedaemon: Missing response to command %s\n", command);
         ei_x_format_wo_ver(&response, "{response, ~l, {error, \"missing response\"}}", reqId);        
     }
@@ -156,30 +185,36 @@ void handle_erl()
 
 int main()
 {
-    if(!allo_initialize(false)) {
+    if(!allo_initialize(false))
+    {
         fprintf(stderr, "Unable to initialize allostate");
         return -1;
     }
 
     ei_init();
 
-    if(fcntl(erlin, F_SETFL, O_NONBLOCK) != 0) {
+    if(fcntl(erlin, F_SETFL, O_NONBLOCK) != 0)
+    {
         perror("failed to set erlin as non-blocking");
         return -4;
     }
     
     printf("allostateport open as %d\n", getpid());
     
-    while (1) {
+    while (1)
+    {
         ENetSocketSet set;
         ENET_SOCKETSET_EMPTY(set);
         ENET_SOCKETSET_ADD(set, erlin);
         
         int selectr = enet_socketset_select(erlin, &set, NULL, 100);
-        if(selectr < 0) {
+        if(selectr < 0)
+        {
             perror("select failed, terminating");
             return -3;
-        } else if(ENET_SOCKETSET_CHECK(set, erlin)) {
+        }
+        else if(ENET_SOCKETSET_CHECK(set, erlin))
+        {
             handle_erl();
         }
     }
