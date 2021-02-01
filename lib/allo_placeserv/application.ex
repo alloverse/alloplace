e6886725dfc898a925f32462ec5ae2424eeb8fdf
Nevyn Bengtsson <nevyn@alloverse.com>
2021-02-01 10:54:58 +0100

    outer supervisor so we serve forever

--- lib/allo_placeserv/application.ex
+++ lib/allo_placeserv/application.ex
@@ -5,10 +5,38 @@ defmodule AlloPlaceserv.Application do
   use Application
 
   def start(_type, _args) do
-    # If we're not running on a properly provisioned server, make sure we have a dummy
-    # storage folder available.
+    # If we're not running on a properly provisioned server, make sure we have dummy
+    # tmp and storage folders available.
     File.mkdir("storage")
+    File.mkdir("tmp")
 
+    #:debugger.start()
+    #:observer.start()
+
+    # Setup a root supervisor that just makes sure we serve basically forever, even if
+    # everything keeps crashing
+    Supervisor.start_link([
+      AlloPlaceserv.MainSupervisor
+    ], [
+      strategy: :one_for_one,
+      max_restarts: 100,
+      max_seconds: 10,
+      name: RestartTheWorldSupervisor
+    ])
+  end
+end
+
+defmodule AlloPlaceserv.MainSupervisor do
+  use Supervisor
+  require Logger
+
+  def start_link(init_arg) do
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_init_arg) do
+    Logger.info("Starting main supervisor from scratch")
     # Make sure we start with an empty state, so we don't start from stale state..
     # In the future, we can use the stored information to reconnect clients and continue
     # from this stale state, but we're not there yet.
@@ -23,10 +51,8 @@ defmodule AlloPlaceserv.Application do
       {NetDaemon, [name: NetProc, delegate: ServProc, port: 31337]},
     ]
 
-    #:debugger.start()
-    #:observer.start()
-
-    opts = [strategy: :one_for_one, name: Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.init(children, [
+      strategy: :one_for_one,
+    ])
   end
 end
